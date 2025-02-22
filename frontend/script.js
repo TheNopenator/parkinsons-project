a7034bdfc6c6a0583071beb572678e2e7a613db9
Rick <ricklee2487@gmail.com>
2025-02-22 14:12:06 -0500

    Linked main webpage with location tracker, changed up aesthetics

--- frontend/script.js
+++ frontend/script.js
@@ -57,3 +57,13 @@
             center: userLocation
         });
     }
+
+    function openSidebar() {
+        document.body.classList.add('sidebar-open');
+        document.getElementById("main-content").style.marginLeft = "320px";
+    }
+
+    function closeSidebar() {
+        document.body.classList.remove('sidebar-open');
+        document.getElementById("main-content").style.marginLeft = "0";
+    }
